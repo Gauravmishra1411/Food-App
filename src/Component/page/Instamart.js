10d09604b2cc5ef7e2cece0149d9d33f3f04b491
Gauravmishra <iamgauravdbg@gmail.com>
2025-08-31 11:13:16 +0530

    Accodina

--- src/Component/page/Instamart.js
+++ src/Component/page/Instamart.js
@@ -1,9 +1,17 @@
  import React from 'react'
  
- const Instamart = () => {
+ import  { Section } from './Profile'
+  import  {Profiles } from './Profile'
+  import {   Sidebar } from './Profile'
+
+ const Instamart = ({props}) => {
    return (
-     <div>
-       <h1>100 of instamart inside it</h1>
+     <div className='flex flex-col gap-2 mx-5'>
+ 
+       
+   <Section/>
+   <Profiles/>
+  <Sidebar/>
      </div>
    )
  }
